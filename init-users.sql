876ea698e2e4ab4cc6e612954aa079f453039d9c
jurck222 <56190152+jurck222@users.noreply.github.com>
2024-06-26 10:16:16 +0200

    :card_file_box: Added init file for users database tables

---
+++ init-users.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS _user (
+    id INT PRIMARY KEY,
+    password VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    role SMALLINT
+);
+
+CREATE TABLE IF NOT EXISTS user_services (
+    user_id INT,
+    services SMALLINT,
+    PRIMARY KEY (user_id, services),
+    FOREIGN KEY (user_id) REFERENCES _user(id)
+);
+
+INSERT INTO _user (id, password, first_name, last_name, email, phone, role) VALUES
+(4,'$2a$10$WdX2UxB/q7p1/Hh6kawhRuB6MIhNI7LrFc31CrHkASdflcxaXIL3y', 'Joze', 'Pacient', 'joze@example.com', '555-1234', 1),
+(5,'$2a$10$mNJfF7MVrtawxGsYcNu7Ye8KLX/8ZORZWvRavK3g1lvZEI88upbNS', 'Janka', 'Jovakovic', 'janka@example.com', '555-5678', 0);
+(6,'$2a$10$mNJfF7MVrtawxGsYcNu7Ye8KLX/8ZORZWvRavK3g1lvZEI88upbNS', 'Zeljko', 'Joksimovic', 'zeljko.jok@example.com', '555-8263', 0);
+(7,'$2a$10$WdX2UxB/q7p1/Hh6kawhRuB6MIhNI7LrFc31CrHkASdflcxaXIL3y', 'Corey', 'Taylor', 'corey.tay@example.com', '555-7896', 1),
+
+INSERT INTO user_services (user_id, services) VALUES
+(5, 0),
+(5, 1),
+(5, 2),
+(6, 3),
+(6, 1);
